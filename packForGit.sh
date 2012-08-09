dd72edce9aa3fc1b7daeddb1c69b8049249b1436
root <root@LNDATA2.(none)>
2012-08-09 23:39:41 +0100

    git push via script: test

---
+++ packForGit.sh
@@ -0,0 +1,6 @@
+echo "Packing git commit and push!!!"
+git add *
+echo "Enter commit comment:"
+read commentGit
+git commit -m "git push via script: $commentGit"
+git push -repo git://github.com/martinshaw/youtilities.git
